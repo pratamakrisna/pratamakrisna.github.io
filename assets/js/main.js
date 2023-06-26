592b59c4992ae2716c4019ef2488a6a08e4cc843
Krisna Aditya Pratama <krisnaadityapratama@students.amikom.ac.id>
2023-06-26 23:12:13 +0700

    code repair
    code + js add for password login

--- assets/js/main.js
+++ assets/js/main.js
@@ -196,3 +196,88 @@
   });
 
 })()
+
+//Komdat
+function showPasswordPrompt0() {
+  Swal.fire({
+    title: 'Masukkan Kata Sandi',
+    input: 'password',
+    showCancelButton: true,
+    confirmButtonText: 'Submit',
+    preConfirm: (password) => {
+      if (password === '75439') { // Ganti dengan kata sandi yang diinginkan
+        window.location.href = 'Jkaghalskmsda/kd.html'; // Ganti dengan URL halaman lain yang diinginkan
+      } else {
+        Swal.fire('Kata Sandi Salah', '', 'error');
+      }
+    }
+  });
+}
+
+//HS
+function showPasswordPrompt1() {
+  Swal.fire({
+    title: 'Masukkan Kata Sandi',
+    input: 'password',
+    showCancelButton: true,
+    confirmButtonText: 'Submit',
+    preConfirm: (password) => {
+      if (password === '18926') { // Ganti dengan kata sandi yang diinginkan
+        window.location.href = 'g8pVozNcU+F=/hs.html'; // Ganti dengan URL halaman lain yang diinginkan
+      } else {
+        Swal.fire('Kata Sandi Salah', '', 'error');
+      }
+    }
+  });
+}
+
+//PW
+function showPasswordPrompt2() {
+  Swal.fire({
+    title: 'Masukkan Kata Sandi',
+    input: 'password',
+    showCancelButton: true,
+    confirmButtonText: 'Submit',
+    preConfirm: (password) => {
+      if (password === '90278') { // Ganti dengan kata sandi yang diinginkan
+        window.location.href = 'JH-YK+dy&xiS/pw.html'; // Ganti dengan URL halaman lain yang diinginkan
+      } else {
+        Swal.fire('Kata Sandi Salah', '', 'error');
+      }
+    }
+  });
+}
+
+//Bd
+function showPasswordPrompt3() {
+  Swal.fire({
+    title: 'Masukkan Kata Sandi',
+    input: 'password',
+    showCancelButton: true,
+    confirmButtonText: 'Submit',
+    preConfirm: (password) => {
+      if (password === '88203') { // Ganti dengan kata sandi yang diinginkan
+        window.location.href = 'WPukZkuhsvNP/bd.html'; // Ganti dengan URL halaman lain yang diinginkan
+      } else {
+        Swal.fire('Kata Sandi Salah', '', 'error');
+      }
+    }
+  });
+}
+
+//So
+function showPasswordPrompt4() {
+  Swal.fire({
+    title: 'Masukkan Kata Sandi',
+    input: 'password',
+    showCancelButton: true,
+    confirmButtonText: 'Submit',
+    preConfirm: (password) => {
+      if (password === '36279') { // Ganti dengan kata sandi yang diinginkan
+        window.location.href = 'M52.PeENVKoN/so.html'; // Ganti dengan URL halaman lain yang diinginkan
+      } else {
+        Swal.fire('Kata Sandi Salah', '', 'error');
+      }
+    }
+  });
+}
